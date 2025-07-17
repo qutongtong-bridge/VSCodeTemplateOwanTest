29894321ccf294e367d3e3c26a76af2c140eee13
qutongtong-bridge <qutongtong@bridge.vc>
2025-07-17 06:07:31 +0000

    添加 src/pages/ProductCategory.js

---
+++ Qrmarketing/src/pages/ProductCategory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card, Empty } from 'antd';
+
+const ProductCategory = () => {
+  return (
+    <Card title="产品参数设置">
+      <Empty description="产品参数设置功能开发中" />
+    </Card>
+  );
+};
+
+export default ProductCategory;
