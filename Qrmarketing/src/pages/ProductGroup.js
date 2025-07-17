06599b9e247e08a8a0a30be8d2a76c189d87baee
qutongtong-bridge <qutongtong@bridge.vc>
2025-07-17 06:07:30 +0000

    添加 src/pages/ProductGroup.js

---
+++ Qrmarketing/src/pages/ProductGroup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card, Empty } from 'antd';
+
+const ProductGroup = () => {
+  return (
+    <Card title="产品参数分组">
+      <Empty description="产品参数分组功能开发中" />
+    </Card>
+  );
+};
+
+export default ProductGroup;
