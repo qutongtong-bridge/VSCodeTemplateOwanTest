a5de6a1c49ff31f502f93cb1c0da89ab3a199268
qutongtong-bridge <qutongtong@bridge.vc>
2025-07-17 06:07:31 +0000

    添加 src/pages/ProductManagement.js

---
+++ Qrmarketing/src/pages/ProductManagement.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card, Empty } from 'antd';
+
+const ProductManagement = () => {
+  return (
+    <Card title="产品管理">
+      <Empty description="产品管理功能开发中" />
+    </Card>
+  );
+};
+
+export default ProductManagement;
